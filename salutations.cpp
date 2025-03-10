f8c1c41e044133f80449851fd3240bee9c2745fa
Adithya S <145419966+Nanashibi@users.noreply.github.com>
2025-03-10 22:32:22 +0530

    Create salutations.cpp

---
+++ salutations.cpp
@@ -0,0 +1,9 @@
+//File: salutations.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Salutations, huzz!\n";
+  cout << "Hope you're having a good day.";
+  return 0;
+}
